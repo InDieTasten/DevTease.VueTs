4d92c889de5cb19647cdae1f2829f8e1093f9256
InDieTasten <indietasten@gmail.com>
2018-10-26 17:52:36 +0200

    Add ManualReport component

---
+++ src/components/ManualReport.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h1> Manual Report </h1>
+    <!-- <ReportTimeSelection v-model="selectedTimeframe" :allowedTimeframe="allowedTimeframe" /> -->
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ManualReport extends Vue {
+  private allowedTimeframe: any;
+  private selectedTimeframe: any;
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
